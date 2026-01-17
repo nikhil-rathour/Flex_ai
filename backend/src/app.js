690577639d18b9a5090f090e70fed05ee19cbe2e
nikhil-rathour <rathournikhil042@gmail.com>
2026-01-17 13:11:31 +0530

    refactor: organize imports and enhance app initialization in app.js and server.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
-import express from  "express"
-const app = express()
+import express from "express";
+import cors from "cors";
+import userRouter from "./auth/authRouter.js"
+import  geminiRouter from "./gemini/geminiRouter.js";
+import paletteRouter from "./palette/paletteRouter.js";
+import codeStorageRouter from "./codeStorage/codeStorageRouter.js";
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/gemini", geminiRouter);
+app.use("/api", paletteRouter);
+app.use("/api/codes", codeStorageRouter);
 
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
 });
 
-
-export default app
+export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
-import app from './app.js'
-import connectDB from "./configs/db.js"
+import app from "./app.js";
+import connectDB from "./configs/db.js";
 import dotenv  from "dotenv"
+
 dotenv.config()
 connectDB()
 const PORT = process.env.PORT || 4000
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
